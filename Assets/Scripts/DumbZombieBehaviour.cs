734db35e87cec265fbebee807095282b876326b3
Kieran <k.bondman7@gmail.com>
2019-12-01 11:39:44 -0600

    collision lives

--- Assets/Scripts/DumbZombieBehaviour.cs
+++ Assets/Scripts/DumbZombieBehaviour.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class DumbZombieBehaviour : MonoBehaviour
 {
     public Transform target;
-    private float speed = 1f;
+    private float speed = 3f;
+    public TextMeshProUGUI remainingText;
+    public int lives = 3;
 
     void Update()
     {
@@ -13,4 +17,17 @@ public class DumbZombieBehaviour : MonoBehaviour
 		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         transform.LookAt(target);
 	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            lives = lives - 1;
+            Debug.Log(lives);
+            if (lives == 0)
+            {
+                Debug.Log("YOUDIE");
+            }
+        }
+    }
 }
