86cf250d6a156932813541cff78614b22776dbec
Jasper Gordon <jgordon3@bowdoin.edu>
2019-12-06 00:25:12 +0100

    Improved Main Menu with Mountain

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Control : MonoBehaviour
+{
+    void NextScene()
+    {
+        SceneManager.LoadScene("PrettyScene", LoadSceneMode.Single);
+    }
+}
