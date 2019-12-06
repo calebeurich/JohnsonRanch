d1bd1884e2474211cf9358ffa216d591e1cdb68a
Jasper Gordon <jgordon3@bowdoin.edu>
2019-12-06 12:09:31 +0100

    Added some animation code but not functioning

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //Basic script to switch scenes
 public class Control : MonoBehaviour
 {
     public string sceneName;
+    public Image black;
+    public Animator anim;
 
-    void NextScene()
+    public void NextScene()
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
+
+   // IEnumerator Fading()
+    //{
+       // anim.SetBool("Fade", true);
+      //  yield return new WaitUntil(()=>black.color.a==1);
+       // SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    //}
+
 }
