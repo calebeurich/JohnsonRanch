40cecddb4dd29653e0d8d827b5860fb09b4d419a
Jasper Gordon <jgordon3@bowdoin.edu>
2019-12-06 11:29:06 +0100

    Added Scenery and Music to Menu

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//By Jasper
+//Purpose of this script is to create a rotator for the main camera
 public class CameraRotator : MonoBehaviour
 {
-    public float speed;
+    public float speed; //adjustable variable to control speed of rotation
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+//Basic script to switch scenes
 public class Control : MonoBehaviour
 {
+    public string sceneName;
+
     void NextScene()
     {
-        SceneManager.LoadScene("PrettyScene", LoadSceneMode.Single);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }
